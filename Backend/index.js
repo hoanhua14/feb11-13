7ece5f93dd1a95a593dc6422c0f08dbf5e0b5819
hoanhua14 <huasohoan@gmail.com>
2026-02-12 21:56:08 -0800

    post req created

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,25 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
 const port = 3001;
 
 app.listen(port, () => console.log(`running on ${port}`));
 
+const tasks = [
+  { id: 1, task: "do dishes", status: "completed" },
+  { id: 2, task: "do laundry", status: "incomplete" },
+  { id: 3, task: "mop the floor", status: "completed" },
+];
 app.get("/", (req, res) => {
-  res.send("running");
+  res.json(tasks);
+});
+app.post("/", (req, res) => {
+  const newTask = {
+    id: tasks.length + 1,
+    task: "mop the room",
+    status: "completed",
+  };
+  tasks.push(newTask);
+  res.status(201).json(tasks);
+  console.log("task created");
 });
